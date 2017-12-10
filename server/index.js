7b62053ea0981799531f4df38d2354bd5f807f6f
RobertPurcea <RobertPurcea@yahoo.com>
2017-12-10 15:40:51 +0200

    changed a bit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ hi: 'changed man changed' });
 })
 
 const PORT = process.env.PORT || 5000;
